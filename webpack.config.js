2c98814e1f658b30373defe0223ea4269a143df3
nicolasgsantana <nicolas07san@gmail.com>
2024-02-19 22:01:16 -0300

    Install html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            title: 'Valhalla Drinks',
+            filename: 'index.html',
+            inject: 'head',
+            scriptLoading: 'defer'
+        }),
+    ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
